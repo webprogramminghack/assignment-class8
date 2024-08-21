a54b2c43c103c70e0540ff94214be6ffedb09a88
haliludin82 <haliludin82@gmail.com>
2024-08-22 05:20:31 +0700

    tugas kelas 8 part 1

--- 1.caching-with-weakmap/index.js
+++ 1.caching-with-weakmap/index.js
@@ -1,20 +1,38 @@
-// please use weakmap to cache the total salary
+//Buat weakmap
+const cache = new WeakMap();
 
+//Buat fungsi salaries
 function processData(salaries) {
-  // start coding here
+  //cek chace
+  if (cache.has(salaries)) {
+    return cache.get(salaries);
+  }
+
+  //hitung total
+  const result = computeTotal(salaries);
+
+  //simpan ke resultnya
+  cache.set(salaries, result);
+
+  return result;
+}
+
+function computeTotal(obj) {
+  //jumlahkan semuanya ke dalam object
+  return Object.values(obj).reduce((total, value) => total + value, 0);
 }
 
 let marketingSalary = { Dan: 1000, Emily: 3000, John: 3000, Kate: 5000 };
 let devSalary = { Alice: 4000, Bob: 6000, Charlie: 8000 };
 
 let result1 = processData(marketingSalary);
-console.log(result1); // Should calculate and return the total salary for the marketing department (12000)
+console.log(result1);
 
 let result2 = processData(devSalary);
-console.log(result2); // Should calculate and return the total salary for the development department (18000)
+console.log(result2);
 
 let result3 = processData(marketingSalary);
-console.log(result3); // Should return the cached total salary for the marketing department (12000)
+console.log(result3);
 
 let result4 = processData(devSalary);
-console.log(result4); // Should return the cached total salary for the development department (18000)
+console.log(result4);
